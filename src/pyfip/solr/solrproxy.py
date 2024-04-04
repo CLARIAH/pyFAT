f00befaa135b06b4251c5c90918b836784ed92d5
wilkos-dans <wilko.steinhoff@dans.knaw.nl>
2024-04-04 17:22:54 +0200

    Added solr proxy
    to bypass BasicAuthentication in DockerSolr

---
+++ src/pyfip/solr/solrproxy.py
@@ -0,0 +1,33 @@
+import socketserver
+import http.server as SimpleHTTPServer
+import urllib
+import urllib.request
+
+from dynaconf import Dynaconf
+
+settings = Dynaconf(settings_files=["conf/settings.toml"], secrets=["conf/.secrets.toml"], environments=True, default_env="default", load_dotenv=True)
+
+PORT = 9098
+class MyProxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        url="http://localhost:8183"+self.path
+
+        passman = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+        passman.add_password(None, url, settings.solr_vlo_usr, settings.solr_vlo_pwd)
+        authhandler = urllib.request.HTTPBasicAuthHandler(passman)
+        opener = urllib.request.build_opener(authhandler)
+        urllib.request.install_opener(opener)
+
+        # res = urllib.request.urlopen(url)
+        # res_body = res.read()
+        # print(res_body.decode('utf-8'))
+        self.headers.add_header('Content-Type', 'application/json; charset=utf-8')
+        self.send_response(200)
+        self.end_headers()
+        self.copyfile(urllib.request.urlopen(url), self.wfile)
+
+
+
+httpd = socketserver.ForkingTCPServer(('', PORT), MyProxy)
+print ("Now serving at "+str(PORT))
+httpd.serve_forever()
